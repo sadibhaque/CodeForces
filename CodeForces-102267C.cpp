e7baeecdd127a8d5e86c9592efc97c3d0a474140
Sadib <111662359+sadibhaque@users.noreply.github.com>
2023-09-11 15:28:55 +0600

    Create CodeForces-102267C.cpp

---
+++ CodeForces-102267C.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long int
+
+int main(){
+    ll s,x,ctr = 0;
+    cin>>s>>x;
+
+    while(s != 0){
+        s = s/x;
+        ctr++;
+    }
+
+    cout<<ctr;
+}
