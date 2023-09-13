30f254813b418385af17f4f6b44b439ba37f5993
Sadib <111662359+sadibhaque@users.noreply.github.com>
2023-09-13 09:08:47 +0600

    Create CodeForces-977A.cpp
    1

---
+++ CodeForces-977A.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long int
+void input(){
+
+    freopen("/home/mendax/Workspace/Temp /input.txt","r",stdin);
+    freopen("/home/mendax/Workspace/Temp /output.txt","w",stdout);
+
+}
+
+int main(){
+    input();
+    ll n,k;
+    cin>>n>>k;
+
+    for (int i = 0; i < k; i++){
+        if(n % 10 == 0) n /= 10;
+        else n--;
+    }
+
+    cout<<n<<endl;
+}
